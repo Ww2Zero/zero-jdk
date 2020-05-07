5cd1505b1b0eaecdee9ffd1a972c9d88166cec18
wuaq <wuaq@szkingdom.com>
2020-05-07 16:22:00 +0800

    feat(source): init

---
+++ src/com/zero/test/test.java
@@ -0,0 +1,38 @@
+/**
+ * <p>文件名称: test.java</p>
+ * <p>文件描述: </p>
+ * <p>版权所有: 版权所有(C)2016-</p>
+ * <p>公   司: 金证财富南京科技有限公司</p>
+ * <p>内容摘要: </p>
+ * <p>其他说明: </p>
+ * <p>创建日期: 2020/4/2 9:15 上午 </p>
+ * <p>完成日期: </p>
+ * <p>修改记录1:</p>
+ * <pre>
+ *    修改日期：
+ *    版 本 号：
+ *    修 改 人：
+ *    修改内容：
+ * </pre>
+ * <p>修改记录2：…</p>
+ *
+ * @version 1.0
+ * @author wuaq@szkingdom.com
+ */
+
+package com.zero.test;
+
+import java.util.HashMap;
+
+public class test {
+
+    public static void main(String[] args) {
+        HashMap<String,String> hashMap = new HashMap<>();
+
+        hashMap.put("1","1");
+        hashMap.put("2","1");
+        hashMap.put("3","1");
+        hashMap.put("4","1");
+        hashMap.put("5","1");
+    }
+}
