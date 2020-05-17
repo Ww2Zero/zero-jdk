7cf880e6a3dd769e8ff5cba20e8fc31835b2acd1
Ww2Zero <wwzerome@gmail.com>
2020-05-17 16:24:39 +0800

    test(enum): test

---
+++ src/com/zero/test/java/lang/enumm/Color.java
@@ -0,0 +1,29 @@
+/**
+ * <p>文件名称: Color.java</p>
+ * <p>文件描述: </p>
+ * <p>版权所有: 版权所有(C)2016-</p>
+ * <p>公   司: 金证财富南京科技有限公司</p>
+ * <p>内容摘要: </p>
+ * <p>其他说明: </p>
+ * <p>创建日期: 2020/5/16 3:11 下午 </p>
+ * <p>完成日期: </p>
+ * <p>修改记录1:</p>
+ * <pre>
+ *    修改日期：
+ *    版 本 号：
+ *    修 改 人：
+ *    修改内容：
+ * </pre>
+ * <p>修改记录2：…</p>
+ *
+ * @version 1.0
+ * @author wuaq@szkingdom.com
+ */
+
+package com.zero.test.java.lang.enumm;
+
+public enum Color {
+    RED,
+    WHITE,
+    BLACK
+}
---
+++ src/com/zero/test/java/lang/enumm/Test.java
@@ -0,0 +1,10 @@
+package com.zero.test.java.lang.enumm;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Color color = Color.RED;
+        System.out.println("color = " + color);
+    }
+
+}
